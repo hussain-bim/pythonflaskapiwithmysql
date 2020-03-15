27f92b83bbb6fc2fb79a27dd7d7947a2e6f4b23c
hussain-bim <37952588+hussain-bim@users.noreply.github.com>
2020-03-15 09:00:26 +0800

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,13 @@
+Objective of this application
+
+1. Create user database and be able to add users, edit or delete
+2. Use Python Flask as API end point
+3. Use simple HTML for front end
+4. USe MySQL as back end database
+
+Implementation steps
+
+1. Use XAMPP to import the MySQL Database dump 'wintiptoys.sql'
+2. Open the project 'user_crud_v3' in PyCharm or other Python editors
+3. Run main.py
+4. The application will start on http://127.0.0.1:5000/
---
+++ wingtiptoys.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2020 at 01:51 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wingtiptoys`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_names` (IN `id_input` INT)  BEGIN
+select * from pet where id = id_input;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SELECT_FROM_TBL_V3` (IN `tbl_name` VARCHAR(200))  NO SQL
+BEGIN
+ SET @t1 =CONCAT('SELECT * FROM ',tbl_name );
+ PREPARE stmt3 FROM @t1;
+ EXECUTE stmt3;
+ DEALLOCATE PREPARE stmt3;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pet`
+--
+
+CREATE TABLE `pet` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `species` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pet`
+--
+
+INSERT INTO `pet` (`id`, `name`, `species`) VALUES
+(1, 'Dog', 'Domestic'),
+(2, 'Cat', 'Domestic'),
+(3, 'Fish', 'Domestic');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `user_id` bigint(20) NOT NULL,
+  `user_name` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_email` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `user_name`, `user_email`, `user_password`) VALUES
+(4, '1', '1', 'pbkdf2:sha256:150000$tKgEnE3p$118a4c441454017cef317de7604522ad69318afd1a464bb7a99c152b1605f4d0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
